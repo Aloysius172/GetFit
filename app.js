ed6b4bacd54574f7013a724b6f5a5bc356f1d3c2
“Aloysius <aloysius.ekezie1@gmail.com>
2022-08-28 13:00:37 -0400

    modified routes and dependencies

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
 const mongoose = require('mongoose');
+const users = require("./routes/api/users");
+const regimens = require("./routes/api/regimens");
+const bodyParser = require('body-parser');
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -13,3 +16,9 @@ app.get("/", (req, res) => res.send("Getfit"));
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+app.use("/api/users", users);
+app.use("/api/regimens", regimens);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
