bb614f50e397f02f94aa581f93ad4f942b387b98
“Aloysius <aloysius.ekezie1@gmail.com>
2022-08-28 14:03:22 -0400

    password encryption and handling email errors

--- app.js
+++ app.js
@@ -5,13 +5,28 @@ const mongoose = require('mongoose');
 const users = require("./routes/api/users");
 const regimens = require("./routes/api/regimens");
 const bodyParser = require('body-parser');
+const User = require('./models/User');
+
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Getfit"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  // const user = new User({
+  //   username:"Freddy",
+  //   email: "fredon@gmail.com",
+  //   password: "123456"
+
+  // }) 
+  // user.save()
+  res.send("Getfit");
+})
+
 
 const port = process.env.PORT || 3000;
 
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,39 @@
 const express = require("express");
 const router = express.Router();
+const User = require('../../models/User');
+const bcrypt = require('bcryptjs');
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
+
+router.post('/register', (req, res) => {
+  // const { errors, isValid } = validateRegisterInput(req.body);
+
+  // if (!isValid) {
+  //   return res.status(400).json(errors);
+  // }
+  User.findOne({ email: req.body.email })
+  .then(user => {
+    if (user) { 
+      return res.status(400).json({ email: "A user is already registered with that email" }) 
+    } else {
+      const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+      })
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser.save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        })
+      })
+    }    
+  })
+})
+
+
 
 module.exports = router;
