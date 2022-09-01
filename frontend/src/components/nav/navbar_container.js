2a04797cebb357202091fe9270944c325d77f543
stachecoding2 <stachecoding2@DESKTOP-0BN2DDD.localdomain>
2022-09-01 12:54:04 -0400

    navbar login complete

--- frontend/src/components/nav/navbar_container.js
+++ frontend/src/components/nav/navbar_container.js
@@ -1,6 +1,7 @@
 import { connect } from 'react-redux';
 import { logout } from "../../actions/session_actions";
 import { openModal } from '../../actions/modal_actions';
+import { login } from '../../actions/session_actions';
 
 import NavBar from './navbar';
 
@@ -9,8 +10,14 @@ const mapStateToProps = state => ({
     username: state.session.user.username
 });
 
+const mapDispatchToProps = dispatch => ({
+    openModal: modal => dispatch(openModal(modal)),
+    processDemoLogin: (user) => dispatch(login(user)),
+    logout: () => dispatch(logout()),
+    login: (user) => dispatch(login(user))
+});
+
 
 export default connect(
-    mapStateToProps,
-    { logout }
+    mapStateToProps, mapDispatchToProps
 )(NavBar);
