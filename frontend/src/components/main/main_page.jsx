f8ae958e49da8467ea8f09350e3276a1a71d435b
jrogergordon <103604859+jrogergordon@users.noreply.github.com>
2022-09-01 11:07:51 -0400

    Revert "splash css"

--- frontend/src/components/main/main_page.jsx
+++ frontend/src/components/main/main_page.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import MainStyling from './main_page.css'
-import { Link } from 'react-router-dom'
 
 
 
@@ -10,89 +8,16 @@ const Greeting = ({ currentUser, logout, openModal, processDemoLogin }) => {
 
   const sessionLinks = () => (
     <div className="greeting">
-      <div id="landing">
+      <div id="greeting">
+        <h1 className="tumbler">GetFit</h1>
+        <nav className="greeting-logged-out">
 
-
-        <div id="arm-left">
-          <a href="http://localhost:3000/#/exercises">
-            <div id="arm-left-link" ></div>
-          </a>
-        </div>
-
-        <div id="arm-left-blurb">
-          <p className='empty-blurb'>There are plenty of muscle groups in your
-          arms, like your biceps and triceps. Click now to look at some ways you can exercise them!</p>  
-        </div>
-
-
-        <div id="arm-right">
-          <a href="http://localhost:3000/#/exercises">
-            <div id="arm-right-link"></div>
-          </a>
-        </div>
-
-        <div id="arm-right-blurb">
-          <p className='empty-blurb'> There are plenty of muscle groups in your
-            arms, like your biceps and triceps.Click now to look at some ways you can exercise them! </p>
-        </div>
-
-
-        <div id="chest">
-          <a href="http://localhost:3000/#/exercises">
-            <div id="chest-link"></div>
-          </a>
-        </div>
-        
-        <div id="chest-blurb">
-          <p className='empty-blurb'>There are plenty of muscle groups in your
-            chest, like your pectorals and your deltoids.Click now to look at some ways you can exercise them!</p>  
-        </div>
-
-
-        <div id="abs">
-          <a href="http://localhost:3000/#/exercises">
-            <div id="abs-link"></div>
-          </a>
-        </div>
-
-        <div id="abs-blurb">
-          <p className='empty-blurb'>There are plenty of muscle groups in your
-            abs, like your internal and external obliques.Click now to look at some ways you can exercise them!</p>  
-        </div>
-
-
-        <div id="leg-left">
-          <a href="http://localhost:3000/#/exercises">
-            <div id="leg-left-link"></div>
-          </a>
-        </div>
-        <div id="leg-left-blurb">
-          <p className='empty-blurb'>There are plenty of muscle groups in your
-            legs, like your quadriceps, and your hamstrings.Click now to look at some ways you can exercise them!</p>  
-        </div>
-
-
-        <div id="leg-right">
-          <a href="http://localhost:3000/#/exercises">
-            <div id="leg-right-link"></div>
-          </a>
-        </div>
-        
-        <div id="leg-right-blurb">
-          <p className='empty-blurb'>There are plenty of muscle groups in your
-            legs, like your quadriceps, and your hamstrings.Click now to look at some ways you can exercise them!</p>  
-        </div>
-      </div>
-      <div id="landing-intro">
-        <p className='empty-blurb'>Hello! Welcome to GetFit, a fitness app to help you 
-        cultivate workout regimens for yourself and others. Head to our "exercises" tab
-        to see info on some general exercises, and click on our "regiments" link to create
-        a workout regiment with those exercises. Here on our landing page you can interact
-        with our bodu diagram and see which exercises suit which muscle groups. Have a fit day!
-        </p>
+          <button id="greeting-login" onClick={() => openModal('login')}>Login</button>
+          <button id="greeting-signup" onClick={() => openModal('signup')}>Signup</button>
+          <button className='demo-login' onClick={() => processDemoLogin({email: "tester@t.com", password: "tester1"})}>Demo</button>
+        </nav>
       </div>
     </div>
-    
   );
 
   return sessionLinks();
