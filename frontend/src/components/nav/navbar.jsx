11e00fbdaf3a937377feccac3cb85e917dd53727
Jacob <jrogergordon@gmail.com>
2022-09-01 21:23:44 -0400

    styled nav

--- frontend/src/components/nav/navbar.jsx
+++ frontend/src/components/nav/navbar.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { Link, Redirect } from 'react-router-dom'
 import { openModal } from '../../actions/modal_actions';
 import './navbar.css';
-
+import { BiLogIn } from '@react-icons/all-files/bi/BiLogIn'
+import { RiAccountCircleLine } from '@react-icons/all-files/ri/RiAccountCircleLine'
+import { RiAccountCircleFill } from '@react-icons/all-files/ri/RiAccountCircleFill'
+import { FaRunning } from '@react-icons/all-files/fa/FaRunning'
+import { CgGym } from '@react-icons/all-files/cg/CgGym'
 
 class NavBar extends React.Component {
     constructor(props) {
@@ -41,20 +45,52 @@ class NavBar extends React.Component {
         if (this.props.loggedIn) {
             return (
                 <div className='logged-in-links'>
-                    <Link className="nav-button" to={'/exercises'}>Exercises</Link>
-                    <Link className="nav-button" to={'/regimens'}>Regimens</Link>
-                    <Link className="nav-button" to={'/calendar'}> Calendar</Link>
-                    <Link className="nav-button" to={'/users'}>{this.props.username}</Link>
+                    <Link className="nav-button" to={'/exercises'}>
+                        <button className="button">Exercises</button>
+                    </Link>
+
+                    <Link className="nav-button" to={'/regimens'}>
+                        <button className="button">
+                        <div>image</div>
+                        <div>Regimens</div>
+                        </button>
+                    </Link>
+
+                    <Link className="nav-button" to={'/calendar'}> 
+                        <button className="button">
+                        <div>image</div>
+                        <div>Calendar</div>
+                        </button>
+                    </Link>
+                    <Link className="nav-button" to={'/users'}>
+                        <button className="button">{this.props.username}</button>
+                    </Link>
                     <button className="nav-button-logout" onClick={this.logoutUser}>Logout</button>
                 </div>
             );
         } else {
             return (
                 <div className='logged-out-links'>
-                    <Link className="nav-button" to={'/exercises'}>Exercises</Link>
-                    <button className="login-button" id="greeting-login" onClick={() => this.props.openModal('login')}>Login</button>
-                    <button className="login-button" id="greeting-signup" onClick={() => this.props.openModal('signup')}>Signup</button>
-                     <button className='demo-login' onClick={() => this.handleDemoLogin()}>Demo User</button>
+                    <Link className="nav-button" to={'/exercises'}>
+                        <button className="button" id="exercises-button">
+                            <div id="exercises-icon"><CgGym size={30}/></div>
+                            <div id="exercises-word">Exercises</div>
+                        </button>    
+                    </Link>
+                    <button className="button" id="greeting-login" onClick={() => this.props.openModal('login')}>
+                       <div id="login-icon">< BiLogIn size={30} /></div>  
+                        <div id="login-word">Login</div>    
+                    </button>
+                    
+                    <button className="button" id="greeting-signup" onClick={() => this.props.openModal('signup')}>
+                        <div id="signup-icon"><RiAccountCircleLine size={30}/></div>
+                        <div id="signup-word">Signup</div> 
+                    </button>
+
+                     <button className='button' id="demo-login-button" onClick={() => this.handleDemoLogin()}>
+                        <div id="demo-login-icon">< RiAccountCircleFill size={30}/></div>
+                        <div id="demo-login-word">Demo</div> 
+                    </button>
                     {/* <Link to={'/signup'}>Signup</Link>
                     <Link to={'/login'}>Login</Link> */}
                 </div>
@@ -65,7 +101,11 @@ class NavBar extends React.Component {
     render() {
         return (
             <div className='navbar-div'>
-                <Link to='/'><h1>GetFit</h1></Link>
+                <Link to='/'><button className='button' id="splash-button">
+                    <div><FaRunning size={30}/></div>
+                     <div>GetFit</div> 
+                </button>
+                </Link>
                 { this.getLinks() }
             </div>
         );
