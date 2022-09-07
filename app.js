395127f17997abcd0845ed19c93821ef9c414cdc
stachecoding2 <stachecoding2@DESKTOP-0BN2DDD.localdomain>
2022-09-07 10:12:15 -0400

    dev and prod keys setup

--- app.js
+++ app.js
@@ -44,3 +44,14 @@ app.use("/api/likes", likes);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+//Deployment code for Heroku (Production Build)
+
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
