b35edfafac2a55614070588b943d5e212afc0718
Jacob <jrogergordon@gmail.com>
2022-09-13 19:18:03 -0400

    css-regi-idx

--- frontend/src/components/regimens/regimen_index_item.jsx
+++ frontend/src/components/regimens/regimen_index_item.jsx
@@ -7,23 +7,61 @@ class RegimenIndexItem extends React.Component{
   constructor(props){
     super(props)
   }
+
+  componentDidMount() {
+    this.props.fetchUser(this.props.regimen.user_id);
+  }
   
 
   render(){
 
-    let muscles = this.props.regimen.exercise_ids.map(exercise => exercise.muscle ? exercise.muscle : null)
+    let muscles = this.props.regimen.exercise_ids.map(exercise => exercise.muscle ? exercise.muscle + " " : " ");
+    let exercises = this.props.regimen.exercise_ids.map(exercise => exercise.name ? exercise.name + ", \n": " ");
+    let uniqueMuscles = [...new Set(muscles)];
+    let user = "Steve"
+    let muscTitle = "Muscle Groups: "
     
-    let uniqueMuscles = [...new Set(muscles)]
+
+
+    // let user = this.props.regimen.creator
+
+
 
 
 
     return(
       <div className="regimen-index-item-container">
-        <Link className="regimen-index-item-title" to={`/regimens/${this.props.regimen._id}`}>
-          {this.props.regimen.title}
-        </Link>
-        <div className='regimen-index-item-muscles'>
-          {uniqueMuscles}
+        <div className='regi-idx-title-auth'>
+          <div> Regimen: &nbsp;
+            <Link className="regimen-index-item-title" to={`/regimens/${this.props.regimen._id}`}>
+              {this.props.regimen.title}
+            </Link>
+          </div>
+          <div className='regi-auth'>
+            Creator: &nbsp;
+            {user}
+          </div>
+        </div>
+
+        <div className='regimen-index-item'>
+          <div className='regi-idx-exrc-cont'>
+            <div className='regi-idx-exrc-title'>
+              Exercises:
+            </div>
+            <div className='reg-shw-exrc-list'>
+              {exercises}
+            </div>
+          </div>
+
+          <div className='regi-idx-musc-cont'>
+            <div className='regi-idx-musc-title'>
+              {muscTitle}
+            </div>
+            <div className='reg-shw-musc-list'>
+              {uniqueMuscles}
+            </div>
+          </div>
+
         </div>
       </div>
     )
--- frontend/src/components/regimens/regimens.jsx
+++ frontend/src/components/regimens/regimens.jsx
@@ -17,16 +17,74 @@ class Regimens extends React.Component {
     }
 
     render () {
-        if(this.props.regimens && this.props.users) 
+        if(this.props.regimens && this.props.users) {
+        let regimens = this.props.regimens.map(regimen => regimen)
+            let colOne;
+            let colTwo;
+            let colThree;
+            if (regimens.length > 0) {
+
+                let col1 = [];
+                let col2 = [];
+                let col3 = [];
+                for (let i = 0; i < regimens.length; i++) {
+                    let temp = i % 4;
+                    if (temp === 0) {
+                        col1.push(regimens[i]);
+                    } else if (temp === 1) {
+                        col2.push(regimens[i]);
+                    } else if (temp === 2) {
+                        col3.push(regimens[i]);
+                    }
+                }
+                colOne = col1.map((regimen, i) => {
+                        return(
+                            <RegimenIndexItemContainer
+                                key={i} regimen={regimen} users={this.props.users}
+                            />
+                        )
+                });
+                colTwo = col2.map((regimen, i) => {
+                    return (
+                        <RegimenIndexItemContainer
+                            key={i} regimen={regimen} users={this.props.users}
+                        />
+                    )
+                });
+                colThree = col3.map((regimen, i) => {
+                    return (
+                        <RegimenIndexItemContainer
+                            key={i} regimen={regimen} users={this.props.users}
+                        />
+                    )
+                });
+                 
+            }
+
+
         return (
             <div className="regimens-index-container">
                 <div className="regimens-index-intro">
-                    Welcome to the Regimens Page!
-                    <Link className="create-regimen-link" to={'/regimens/create'}>Create a new Regimen!</Link>
+                    Welcome to the Regimens Page.
+                    <br />
+                    Below you can check out all the Regimen's created by our users!
+                    <Link className="create-regimen-link" to={'/regimens/create'}>
+                        Create a new Regimen!
+                    </Link>
                 </div>
                 <div className="create-regimen-item-div">
-                    {this.props.regimens.map((regimen, i) => 
-                    <RegimenIndexItemContainer key={i} regimen={regimen} users={this.props.users} />)}
+                    <div className="col-1">
+                        <div className="col-1-spacer"/>
+                        {colOne}
+                    </div>
+                    <div className="col-2">
+                        <div className="col-2-spacer" />
+                        {colTwo}
+                    </div>
+                    <div className="col-3">
+                        <div className="col-3-spacer" />
+                        {colThree}
+                    </div>
                 </div>
                 {/* <div>
                     {this.props.users.map(user => user.email)}
@@ -34,6 +92,7 @@ class Regimens extends React.Component {
             </div>
 
         )
+    }
     }
 }
 
